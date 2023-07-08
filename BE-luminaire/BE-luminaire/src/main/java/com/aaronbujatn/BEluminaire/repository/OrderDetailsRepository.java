e37b760c7b901dc1ee0c85384aa6dd7d53466aa7
Aaron Bujatin <aaronbujatin@gmail.com>
2023-07-08 19:08:19 +0800

    Delete BE-luminaire/BE-luminaire directory

--- BE-luminaire/BE-luminaire/src/main/java/com/aaronbujatn/BEluminaire/repository/OrderDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aaronbujatn.BEluminaire.repository;
-
-import com.aaronbujatn.BEluminaire.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailsRepository extends JpaRepository<Order, Long> {
-
-
-}
--- BE-luminaire/BE-luminaire/src/main/java/com/aaronbujatn/BEluminaire/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aaronbujatn.BEluminaire.repository;
-
-import com.aaronbujatn.BEluminaire.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
